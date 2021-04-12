acef6ef058b094bdb1d8a9fd68a11c1219f0f7d3
Cristian Fuentes Salgado <cfuentess2010@gmail.com>
2021-04-11 21:11:19 -0400

    Modifica script

---
+++ php74.sh
@@ -0,0 +1,6 @@
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-add-repository ppa:ondrej/php -y
+sudo apt-get install -y --allow-change-held-packages \
+php7.4 php7.4-bcmath php7.4-bz2 php7.4-cgi php7.4-cli php7.4-common php7.4-curl php7.4-dba php7.4-dev \
